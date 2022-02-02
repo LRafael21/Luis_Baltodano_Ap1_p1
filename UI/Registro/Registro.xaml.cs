2ad223940ef78f8f5bde48d92538e3da1576f929
Luis <luis_baltodano@ucne.edu.do>
2022-02-02 01:46:41 -0400

    ModificandoElRegistro

---
+++ UI/Registro/Registro.xaml.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Luis_Baltodano_Ap1_p1.UI
+{
+
+
+    public partial class Registro : Window
+    {
+      
+
+        public Registro()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Cargar()
+        {
+
+        }
+
+        private void Limpiar()
+        {
+
+        }
+
+        private bool Validar()
+        {
+            bool esValido = false;
+
+
+            return esValido;
+        }
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
