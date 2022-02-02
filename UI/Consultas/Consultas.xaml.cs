6168ebfea972e77e80912f8a44abae4978e24222
Luis <luis_baltodano@ucne.edu.do>
2022-02-02 01:53:46 -0400

    AgregandoVentanaConsultaYEventoBuscar

---
+++ UI/Consultas/Consultas.xaml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Luis_Baltodano_Ap1_p1.UI
+{
+    public partial class cLibros : Window
+    {
+
+        public cLibros()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+
+
+
+    }
+}
