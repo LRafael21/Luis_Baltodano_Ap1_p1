ba63074cb34f5975b12de457b53f56a33707bb7f
Luis <luis_baltodano@ucne.edu.do>
2022-02-07 18:17:13 -0400

    AgregandoEntidadALaDAL

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,8 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Parcial.Entidades;
 
 public class Contexto:DbContext
 {
-    
+
+    public DbSet<Productos> Productos { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite(@"Data Source=Data\Libros.db");
