985a51ef6da9205892da217186e168055b85ce31
Luis <luis_baltodano@ucne.edu.do>
2022-02-07 19:47:29 -0400

    ArreglandoExistenciaEntidades

--- Entidades/Productos.cs
+++ Entidades/Productos.cs
@@ -1,14 +1,17 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Parcial.Entidades
 {
-
+   
     public class Productos
     {
         //ProductoId, Descripcion, Existencia, Costo y ValorInventario
+        [Key]
         public int ProductoId { get; set; }
+
         public string Descripcion { get; set; }
-        public bool Existencia { get; set; }
+        public int  Existencia { get; set; }
         public int Costo { get; set; }
         public int ValorInventario { get; set; }
 
