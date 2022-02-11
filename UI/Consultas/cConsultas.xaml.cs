7468c82133193fddceeaa73fe1f56e2832147664
Luis <luis_baltodano@ucne.edu.do>
2022-02-10 23:46:38 -0400

    AgregadoMaterialDesing

--- UI/Consultas/cConsultas.xaml.cs
+++ UI/Consultas/cConsultas.xaml.cs
@@ -4,6 +4,7 @@ using Parcial.BLL;
 using System.Linq;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System;
 
 //PUSH
 
@@ -11,7 +12,7 @@ namespace Parcial.UI.Consultas
 {
     public partial class cProductos : Window
 {
-
+        private Productos Producto = new Productos();
     public cProductos()
     {
         InitializeComponent();
@@ -31,6 +32,14 @@ namespace Parcial.UI.Consultas
                 {
                     case 0:
                         listado = ProductosBLL.GetList(e => e.Descripcion.Contains(CriterioTextBoxx.Text));
+                        CriterioTextBoxx.Text.ToLower();
+                        Cargar();
+                        break;
+
+                    case 1:
+                        listado = ProductosBLL.GetList(p => p.ProductoId == Convert.ToInt32(CriterioTextBoxx.Text));
+                        CriterioTextBoxx.Text.ToLower();
+                        Cargar();
                         break;
 
                 }
@@ -41,6 +50,12 @@ namespace Parcial.UI.Consultas
             }
         }
 
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = this.Producto;
+
+        }
        
 
 
